2dad62252c4b1e8d5addbc81f78b77b9129e43a2
Ally399 <39175229+Ally399@users.noreply.github.com>
2021-09-20 13:47:33 -0400

    Update quizomatic.py

--- quizOMatic/quizomatic.py
+++ quizOMatic/quizomatic.py
@@ -142,7 +142,7 @@ def promptMultiQ(metaQuestion, debug ):
     # Then I need to add a function where it asks the user for an answer. 1-4. It then compares that answer to the real answer. 
     userAnswer = input("Choose an answer with a number 1-4.")
     userAnswer = userAnswer - 1
-    if userAnswer = correctAnswer:
+    if userAnswer == correctAnswer:
         print("Correct!")
     else:
         print("Incorrect.")
@@ -155,7 +155,7 @@ def promptBooleanQ(metaQuestion, debug):
     #Then I need to add a function where it asks the for an answer. True or false. It then compares that answer to the real answer.
     #print(correctAnswer)
     userAnswer = input("Choose True or False")
-    if userAnswer = correctAnswer:
+    if userAnswer == correctAnswer:
         print("Correct!")
     else:
         print("Incorrect.")
